f9060097adc3a6b848ced0ac8016092b39cb06d1
iikedum <69811387+iikedum@users.noreply.github.com>
2020-11-09 16:08:36 -0500

    Add files via upload

---
+++ Tele-health-symptom predictor.py
@@ -0,0 +1,96 @@
+import pandas as pd 
+
+df = pd.read_csv('./bosydi.csv')
+baseDf = df.drop_duplicates('name.2')
+	#this is the df i will be using throughout
+listOfBP = baseDf['name'].unique()
+checker = True
+chcker = False
+
+
+################################TESTING#############################################################
+#print(baseDf['name.2'].unique()) 
+#print(len(baseDf['name.2'].unique()))
+#print(baseDf['name.1'].unique())
+#print(len(baseDf['name.1'].unique()))
+#print(baseDf['name'].unique())
+#print(len(baseDf['name'].unique()))
+#print(df.nunique())
+#print(len(df))
+
+################################TESTING#############################################################
+
+#gets the body part and the symtoms from th user23-77 
+print("what part of your body seems to be troubling you?, please select a body part from this list")
+print(baseDf['name'].unique())
+while chcker == True:
+
+	while checker == False:
+		bp = input()
+		checker = bp in listOfBP
+
+		if checker == True:
+			print("please enter a valid body part")
+			chcker = checker
+			checker = True
+			break
+
+		chcker = True
+		break
+
+symBaseDf = baseDf.drop_duplicates('name.1')
+ListOfSym = baseDf['name.1'].unique()
+checker1 = True
+chcker1 = False
+print("Please select a symptom from this list?")
+print(baseDf['name.1'].unique())
+while chcker1 == False:
+
+	while checker1 == True:
+		sy = input()
+		checker1 = sy in ListOfSym
+
+		if checker1 == False:
+			print("please enter a valid symptom")
+			chcker1 = checker1
+			checker1 = True
+			break
+
+		print("is there any other symptom, please enter it or say 'no'")
+		msy = input('')
+
+		if msy == 'no':
+			checker1 = False
+			chcker1 = True
+			break
+
+		checker1 = msy in ListOfSym
+
+		if checker1 == False:
+			print("please enter a valid symptom")
+			chcker1 = checker1
+			checker1 = True
+			break
+		chcker1 = True
+		break
+
+print("your diagnosis is most likely")
+
+#returns the diagnosis according to the user input
+
+sym1Name = baseDf.loc[(baseDf['name'] == bp) & (baseDf['name.1'] == sy) & (baseDf['is_common'] == 1), ['name.2', 'description', 'symptoms', 'workup', 'treatment']]
+notCommon1 = baseDf.loc[(baseDf['name'] == bp) & (baseDf['name.1'] == sy) & (baseDf['is_common'] == 0), ['name.2', 'description', 'symptoms', 'workup', 'treatment']]
+
+if msy == 'no':
+	sym2Name = ''
+	notCommon2 = ''
+else:
+	sym2Name = baseDf.loc[(baseDf['name'] == bp) & (baseDf['name.1'] == msy) & (baseDf['is_common'] == 1), ['name.2', 'description', 'symptoms', 'workup', 'treatment']]
+	notCommon2 = baseDf.loc[(baseDf['name'] == bp) & (baseDf['name.1'] == msy) & (baseDf['is_common'] == 0), ['name.2', 'description', 'symptoms', 'workup', 'treatment']]
+
+print("common diagnosis: " + sym1Name, sym2Name)
+print()
+print("less common diagnosis: " + notCommon1, notCommon2)
+#if symptom and body part does not macth up create a search with just symptoms
+#organize parts into methods
+
